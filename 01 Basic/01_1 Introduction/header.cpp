2e97e1308988c97e2dc0224ebb77f8dd59c999a1
dewwts <xdproduction123@gmail.com>
2025-03-25 11:04:33 +0700

    second

---
+++ 01 Basic/01_1 Introduction/header.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+//#include <bits/stdc++.h> this include all library that we want
+
+#define edge pair<int, int>;
+
+using namespace std;
+
+int main(){
+    //cin.tie(0);
+    //ios_base::sync_with_stdio(false);
+    return 0;
+}
